438c9eeadc1ccb8591208fb87be93f5d88755d02
gsmfnc <francesco.gismondi.f@gmail.com>
2023-05-31 11:23:13 +0200

    fixes

--- autoencoders/paper_plots.m
+++ autoencoders/paper_plots.m
@@ -41,10 +41,8 @@ function bfr = compute_bfr(y, yr)
 num = 0;
 den = 0;
 mean_y = mean(yr);
-for i = 1:1:size(y, 1)
-    num = num + norm(y(i) - yr(i));
-    den = den + norm(yr(i) - mean_y);
-end
+num = norm(y - yr);
+den = norm(yr - mean_y);
 
 bfr = max(1 - num / den, 0) * 100
 end
