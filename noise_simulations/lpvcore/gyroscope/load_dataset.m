48af223c791a3cbdf56036ee03bcc4b10da017a9
gsmfnc <francesco.gismondi.f@gmail.com>
2023-06-22 11:16:30 +0200

    subnets new sims

--- noise_simulations/lpvcore/gyroscope/load_dataset.m
+++ noise_simulations/lpvcore/gyroscope/load_dataset.m
@@ -1,9 +1,9 @@
 function [data_train, data_test, Ts] = load_dataset()
     load("gyroscope_data/ML_estim.mat");
-    exp_p = [cos(exp_p(:, 2)), exp_p(:, 1), sin(exp_p(:, 2))];
+    %exp_p = [cos(exp_p(:, 2)), exp_p(:, 1), sin(exp_p(:, 2))];
     data_train = lpviddata(exp_y, exp_p, exp_u, Ts);
 
     load("gyroscope_data/ML_valid.mat");
-    exp_p = [cos(exp_p(:, 2)), exp_p(:, 1), sin(exp_p(:, 2))];
+    %exp_p = [cos(exp_p(:, 2)), exp_p(:, 1), sin(exp_p(:, 2))];
     data_test = lpviddata(exp_y, exp_p, exp_u, Ts);
 end
